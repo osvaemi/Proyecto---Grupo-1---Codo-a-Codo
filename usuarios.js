56dcf3faa75eccdfd68c0ddd9b36963c97c47004
osvaemi <osvaldoemiliopena@gmail.com>
2022-10-31 13:47:34 -0300

    Terminada

---
+++ usuarios.js
@@ -0,0 +1,29 @@
+function getCharacters(done){
+    const results = fetch("https://rickandmortyapi.com/api/character");
+    results
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+        });
+}
+
+getCharacters(data => {
+
+   data.results.forEach(personaje => {
+
+    const article = document.createRange().createContextualFragment(`
+        <article>
+            <div class='image-container'>
+                <img src='${personaje.image}' alt='Usuario'>
+            </div>
+        
+            <h2>${personaje.name}</h2>
+        </article>
+    `);
+
+    const main = document.querySelector("main");
+    main.append(article);
+   });
+
+});
+
